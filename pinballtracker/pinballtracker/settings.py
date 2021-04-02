5789dd984b0bd7568390d2bdbd7a657b86bd2aba
Sean McGovern <seanmcgovern@Seans-MacBook-Pro-2.local>
2021-04-02 17:16:56 -0400

    Added debug and secret_key environment variables, and set security vars in the settings (such as redirecting http to https)

--- pinballtracker/pinballtracker/settings.py
+++ pinballtracker/pinballtracker/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import django_heroku
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8hp6i*g=y@u$37$t1v0kiq)9)#cvy&8&0b$pir0(angkn5c9%*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+SECURE_SSL_REDIRECT = not DEBUG
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+ALLOWED_HOSTS = ['.pinball-tracker.herokuapp.com']
 
 
 # Application definition
